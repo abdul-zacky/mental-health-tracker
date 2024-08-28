8070436884a099b06cec0ab14ba819750f1e3016
abdul-zacky <zacxvan@gmail.com>
2024-08-28 14:20:34 +0700

    string2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3wk)4r!ah=)foai4mebvbkloy%qu+9lb85b9r8+8-_09k5^-bn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "abdul.zacky-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
